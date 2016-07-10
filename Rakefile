62e091ca1bdd0f1943811a2615f9862462b16b3d
Misigno <efournier92@gmail.com>
2016-07-10 16:16:26 -0400

    delete boilerplate Rails code

--- Rakefile
+++ Rakefile
@@ -1,3 +1,2 @@
 require File.expand_path('../config/application', __FILE__)
-
 RescueMission::Application.load_tasks
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
 RescueMission::Application.config.secret_key_base = '8d29f3e509618e8edc09e4f10d516de8777aab539890a933f34105b4f743805c1353f18db50da4823583ed4770b31558db66a559011958400af39de0b9071083'
